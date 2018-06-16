6ec0d1e4d4989a89bd49607bbb6baca33f8f3a96
Neargye <neargye@gmail.com>
2018-06-16 18:22:15 +0500

    add tensor creat

---
+++ src/creat_tensor.cpp
@@ -0,0 +1,71 @@
+#if defined(_MSC_VER) && !defined(COMPILER_MSVC)
+#  define COMPILER_MSVC // Set MSVC visibility of exported symbols in the shared library.
+#endif
+#include <c_api.h> // TensorFlow C API header
+#include <array>
+#include <iostream>
+#include <vector>
+
+void tensor_deallocation(void *data, size_t len, void *arg) { free(data); }
+
+static void DeallocateTensor(void* data, size_t len, void* arg) {
+  free(data);
+  std::cout << "Deallocate tensor" << std::endl;
+}
+
+int main() {
+  const std::array<int64_t, 3> dims = {1, 5, 12};
+  std::size_t size = sizeof(float);
+  for (auto i : dims) {
+    size *= i;
+  }
+  float* data = static_cast<float*>(malloc(size));
+  std::vector<float> vals = {
+    -0.4809832f, -0.3770838f, 0.1743573f, 0.7720509f, -0.4064746f, 0.0116595f, 0.0051413f, 0.9135732f, 0.7197526f, -0.0400658f, 0.1180671f, -0.6829428f,
+    -0.4810135f, -0.3772099f, 0.1745346f, 0.7719303f, -0.4066443f, 0.0114614f, 0.0051195f, 0.9135003f, 0.7196983f, -0.0400035f, 0.1178188f, -0.6830465f,
+    -0.4809143f, -0.3773398f, 0.1746384f, 0.7719052f, -0.4067171f, 0.0111654f, 0.0054433f, 0.9134697f, 0.7192584f, -0.0399981f, 0.1177435f, -0.6835230f,
+    -0.4808300f, -0.3774327f, 0.1748246f, 0.7718700f, -0.4070232f, 0.0109549f, 0.0059128f, 0.9133330f, 0.7188759f, -0.0398740f, 0.1181437f, -0.6838635f,
+    -0.4807833f, -0.3775733f, 0.1748378f, 0.7718275f, -0.4073670f, 0.0107582f, 0.0062978f, 0.9131795f, 0.7187147f, -0.0394935f, 0.1184392f, -0.6840039f,
+  };
+
+  std::copy(vals.begin(), vals.end(), data); // init input_vals.
+
+  TF_Tensor* tensor = TF_NewTensor(TF_FLOAT,
+                                   dims.data(), static_cast<int>(dims.size()),
+                                   data, size,
+                                   DeallocateTensor, nullptr);
+
+
+  if (TF_TensorType(tensor) != TF_FLOAT) {
+    std::cout << "Wrong tensor type" << std::endl;
+  }
+
+  if (TF_NumDims(tensor) != dims.size())
+
+  {
+    std::cout << "Wrong number of dimensions" << std::endl;
+  }
+
+  for (std::size_t i = 0; i < dims.size(); i++) {
+    if (TF_Dim(tensor, i) != dims[i]) {
+      std::cout << "Wrong dimension size for dim: " << i << std::endl;
+    }
+  }
+  if (TF_TensorByteSize(tensor) != size) {
+    std::cout << "Wrong tensor byte size" << std::endl;
+  }
+
+  auto tf_data = static_cast<float*>(TF_TensorData(tensor));
+
+  for (std::size_t i = 0; i < vals.size(); ++i) {
+    if (tf_data[i] != vals[i]) {
+      std::cout << "Element: " << i << " does not match" << std::endl;
+    }
+  }
+
+  TF_DeleteTensor(tensor);
+
+  std::cout << "Success creat tensor" << std::endl;
+
+  return 0;
+}
