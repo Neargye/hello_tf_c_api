c2ff3c4d1a1bfb6aff3af2922bc6abd88f55bdce
terik23 <neargye@gmail.com>
2019-03-06 12:16:11 +0500

    update license

--- src/tensor_info.cpp
+++ src/tensor_info.cpp
@@ -1,5 +1,6 @@
 // Licensed under the MIT License <http://opensource.org/licenses/MIT>.
-// Copyright (c) 2018 Daniil Goncharov <neargye@gmail.com>.
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2018 - 2019 Daniil Goncharov <neargye@gmail.com>.
 //
 // Permission is hereby  granted, free of charge, to any  person obtaining a copy
 // of this software and associated  documentation files (the "Software"), to deal
